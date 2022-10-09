5a6675a3f215aab9668ef7b04ccea1893a4ca7b6
Minkyu Cho <mk@Minkyus-MBP.fibertel.com.ar>
2022-10-09 16:59:32 -0300

    Subiendo todos los archivos

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+
+const publicPath = path.resolve(__dirname, './public'); 
+app.use(express.static(publicPath) );
+
+app.listen(3030, ()=> {
+        console.log('http://localhost:3030')
+});
+
+app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+        res.sendFile(path.join(__dirname, './views/register.html' ))
+});
+
+app.get('/login', (req, res) => {
+        res.sendFile(path.join(__dirname, './views/login.html' ))
+});
